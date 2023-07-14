610ab78caa6c4f4ed155dbfc4d6d315755e56f66
Luane Von Stein <132390112+LuaneVonStein@users.noreply.github.com>
2023-07-14 16:15:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,38 +6,47 @@ function setupGameBoard() {
 }
 }
 
+function jogarnovamente(){
+const squares = document.getElementsByClassName("square");
+for (let i = 0; i < squares.length; i++){
+  squares[i].textContent = "" ;
+ 
+}
+empate = ["","","","","","","","",""]
+}
 
+
+( verificarEmpate())
 document.addEventListener('DOMContentLoaded', () => {
-    const quadro = document.querySelector("quadro");
+    const quadro = document.querySelector("#quadro");
     const celulas = [];
 
     let currentPlayer = 'X';
-    let isGameOver = false;
+    let fimDeJogo = false;
 
     for (let i = 0; i < 9; i++) {
       const celula = document.createElement("div");
-      celula.listaDeClasse.add("celula");
+      celula.classList.add("celula");
       celulas.push(celula);
       quadro.appendChild(celula);
 
       celula.addEventListener('click', () => {
-        if (isGameOver || celula.textContent !== '') return;
+        if (fimDeJogo || celula.textContent !== '') return;
 
         celula.textContent = jogadoratual;
         celula.style.color = jogadoratual === 'X' ? 'red' : 'blue';
 
-        if (VerificarEmpate()) {
-          isGameOver = true;
-          alert('Empate!');
-          return;
+        if (!jogo.includes("")) {
+          alert("Que pena deu velha ): ");
         }
 
        jogadoratual = jogadoratual === 'X' ? 'O' : 'X';
       });
     }
 
-   
+
     function verificarCombinacao () {
+      
       const verificacaoDeVitoria = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], 
         [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6], 
@@ -57,14 +66,17 @@ document.addEventListener('DOMContentLoaded', () => {
           return true;
         }
       }
-
+      fimDeJogo = true
       return false;
     }
 
     function verificarEmpate () {
-      return [...celulas].every(celula => celulas.textContent !== '');
+      return [...celulas].every(celula => celula.textContent !== '');
     }
   });
+ 
+
+
 
 
 
